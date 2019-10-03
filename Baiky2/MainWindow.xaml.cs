93245a91c403285f311ed1c1118a8676307cd3d7
thuong <thuongnguyen.18062000@gmail.com>
2019-10-03 16:14:46 +0700

    Add project files.

---
+++ Baiky2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Baiky2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int[] mang = { 7, 5, 6, 9, 3 };
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TbnTong_Click(object sender, RoutedEventArgs e)
+        {
+            int tong = 0;
+            foreach (int i in mang)
+                tong += i;
+
+            ketqua.Text = "Tong = " + tong.ToString();
+        }
+
+        private void TbnTongLe_Click(object sender, RoutedEventArgs e)
+        {
+            int tong = 0;
+            foreach (int i in mang)
+                if (i % 2 != 0)
+                    tong += i;
+
+            ketqua.Text = "Tong le = " + tong.ToString();
+        }
+    }
+}
